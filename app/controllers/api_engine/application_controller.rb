44066493970c39a2b0f3b744f4677e4c5d0ec7f2
Steve Faulkner <southpolesteve@gmail.com>
2013-04-13 20:07:08 -0500

    Remove all traces of post from generic controller

--- app/controllers/api_engine/application_controller.rb
+++ app/controllers/api_engine/application_controller.rb
@@ -1,36 +1,36 @@
 module ApiEngine
   class ApplicationController < ActionController::Base
     def index
-      @posts = model_class.all
-      render json: @posts, root: params[:model_name]
+      @models = model_class.all
+      render json: @models, root: plural_model
     end
 
     def show
-      @post = model_class.find(params[:id])
-      render json: @post, root: params[:model_name].singularize
+      @model = model_class.find(params[:id])
+      render json: @model, root: singular_model
     end
 
     def create
-      @post = model_class.new(params[:post])
-      if @post.save
-        render json: @post, status: :created
+      @model = model_class.new(params[singular_model])
+      if @model.save
+        render json: @model, status: :created
       else
-        render json: @post.errors, status: :unprocessable_entity
+        render json: @model.errors, status: :unprocessable_entity
       end
     end
 
     def update
-      @post = model_class.find(params[:id])
-      if @post.update_attributes(params[:post])
+      @model = model_class.find(params[:id])
+      if @model.update_attributes(params[singular_model])
         head :no_content
       else
-        render json: @post.errors, status: :unprocessable_entity
+        render json: @model.errors, status: :unprocessable_entity
       end
     end
 
     def destroy
-      @post = model_class.find(params[:id])
-      @post.destroy
+      @model = model_class.find(params[:id])
+      @model.destroy
       head :no_content
     end
 
@@ -39,5 +39,14 @@ module ApiEngine
     def model_class
       params[:model_name].classify.constantize
     end
+
+    def plural_model
+      params[:model_name]
+    end
+
+    def singular_model
+      params[:model_name].singularize
+    end
+
   end
 end
