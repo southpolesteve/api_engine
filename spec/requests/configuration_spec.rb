b17e646e12d3d99ab3922fbc141431de8fae7542
Steve Faulkner <southpolesteve@gmail.com>
2013-04-21 18:27:02 -0500

    Add config class to ApiEngine module. Pass test from previous commit

--- spec/requests/configuration_spec.rb
+++ spec/requests/configuration_spec.rb
@@ -5,27 +5,27 @@ describe 'Configuration Spec' do
 
     before { Post.create(title: "Test") }
 
-    after(:all) do
+    after do
       ApiEngine.configure do |config|
         config.whitelist = false
       end
     end
 
-    context 'Post is on the whitelist' do
+    context 'Post is not on the whitelist' do
 
       before do
         ApiEngine.configure do |config|
-          config.whitelist = []
+          config.whitelist = [:comment]
         end
       end
 
-      it "does not allow access to the model's API" do
+      it "allows access to the model's API" do
         get '/api/posts'
-        JSON.parse(response.body).should have_key('posts')
+        response.status.should eq(404)
       end
     end
 
-    context 'Post is not on the whitelist' do
+    context 'Post is on the whitelist' do
 
       before do
         ApiEngine.configure do |config|
@@ -35,7 +35,7 @@ describe 'Configuration Spec' do
 
       it "allows access to the model's API" do
         get '/api/posts'
-        JSON.parse(response.body).keys.should have(1).item
+        response.status.should eq(200)
       end
     end
   end
